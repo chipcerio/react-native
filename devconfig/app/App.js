06e24d1ef1a20e4647dc9c3b383fa870b5474fe6
Chip Cerio <alistair.cerio@gmail.com>
2018-09-04 20:53:57 +0800

    updated eslint rules

--- devconfig/app/App.js
+++ devconfig/app/App.js
@@ -6,8 +6,7 @@ const instructions = Platform.select({
   android: `Double tap R on your keyboard to reload,\nShake or press menu button for dev menu`,
 });
 
-type Props = {};
-export default class App extends Component<Props> {
+export default class App extends Component {
   render() {
     return (
       <View style={styles.container}>
