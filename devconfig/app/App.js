1928fb38c60f9665717f1dcda5149ebd06915186
Chip Cerio <alistair.cerio@gmail.com>
2018-09-21 21:29:43 +0800

    husky not working

--- devconfig/app/App.js
+++ devconfig/app/App.js
@@ -1,8 +1,5 @@
 import React, { Component } from 'react';
-import { Platform, StyleSheet, Text, 
-  
-  
-  View } from 'react-native';
+import { Platform, StyleSheet, Text, View } from 'react-native';
 
 const instructions = Platform.select({
   ios: `Press Cmd+R to reload,\nCmd+D or shake for dev menu`,
