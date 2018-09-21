24c21c91e78a50d48428d87deadabaf2c3dec7e8
Chip Cerio <alistair.cerio@gmail.com>
2018-09-21 21:24:57 +0800

    added husky

--- devconfig/app/App.js
+++ devconfig/app/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Platform, StyleSheet, Text, View } from 'react-native';
+import { Button, Platform, StyleSheet, Text, View } from 'react-native';
 
 const instructions = Platform.select({
   ios: `Press Cmd+R to reload,\nCmd+D or shake for dev menu`,
